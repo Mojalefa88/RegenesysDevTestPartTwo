2883b4ed383d213e914045406988ac49c73769a8
Mojalefa Jonas Malatji <mj.malatji@yahoo.com>
2021-08-06 01:24:39 +0200

    first commit

---
+++ Regenesys Dev Part 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Regenesys_Dev_Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] list = { 10.61, 7.92, 6.98, 9.14, 3.13 };
+            double[] tempList = list;
+            Console.Write("Original Array: ");
+            foreach (double i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            // reverse the array
+            Array.Reverse(tempList);
+            Console.Write("Reversed Array: ");
+            foreach (double i in tempList)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
